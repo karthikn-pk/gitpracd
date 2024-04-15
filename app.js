bf6238acaa478c71a254f3b87151d227b869dde5
karthikn-pk <pkarthikeyanp33@gmail.com>
2024-04-15 17:56:29 +0530

    created qr code generator

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const Person=require("./person");
-const person1=new Person("karthikeyan",1988)
-console.log("i am "+person1.name+person1.age()+"years old");
+// const fs = require("fs");
+
+// fs.writeFile("message.txt", "Hello from node js", (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// });
+// readFile('message.txt', 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// var generateName = require("sillyname");
+import generateName from "sillyname";
+import superheroes from "superheroes";
+
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+
+var superheroname = superheroes.random();
+var sillyname = generateName();
+
+// console.log(`my name is ${sillyname}  `);
+// console.log(`my friend name is ${sillyname}`);
+
+console.log(`my name is ${superheroname}  `);
+console.log(`my friend name is ${superheroname}`);
